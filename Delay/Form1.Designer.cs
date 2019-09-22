50278c3d2133fe66f5117cad601905d42cc79e2d
dg <dennis.graiani@gmail.com>
2019-09-22 11:16:14 -0400

    Initial commit

--- Delay/Form1.Designer.cs
+++ Delay/Form1.Designer.cs
@@ -29,11 +29,283 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTarget = new System.Windows.Forms.NumericUpDown();
+            this.txtSpeed = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCurrentDelay = new System.Windows.Forms.Label();
+            this.btnBuild = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnDump = new System.Windows.Forms.Button();
+            this.inputSelector = new System.Windows.Forms.ComboBox();
+            this.outputSelector = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtDumps = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnCough = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.txtTarget)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtSpeed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDumps)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Target Delay";
+            // 
+            // txtTarget
+            // 
+            this.txtTarget.DecimalPlaces = 3;
+            this.txtTarget.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.txtTarget.Location = new System.Drawing.Point(86, 13);
+            this.txtTarget.Name = "txtTarget";
+            this.txtTarget.Size = new System.Drawing.Size(120, 20);
+            this.txtTarget.TabIndex = 1;
+            this.txtTarget.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.txtTarget.ValueChanged += new System.EventHandler(this.txtTarget_ValueChanged);
+            // 
+            // txtSpeed
+            // 
+            this.txtSpeed.Location = new System.Drawing.Point(86, 40);
+            this.txtSpeed.Maximum = new decimal(new int[] {
+            99,
+            0,
+            0,
+            0});
+            this.txtSpeed.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.txtSpeed.Name = "txtSpeed";
+            this.txtSpeed.Size = new System.Drawing.Size(120, 20);
+            this.txtSpeed.TabIndex = 2;
+            this.txtSpeed.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.txtSpeed.ValueChanged += new System.EventHandler(this.txtSpeed_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Speed";
+            // 
+            // lblCurrentDelay
+            // 
+            this.lblCurrentDelay.AutoSize = true;
+            this.lblCurrentDelay.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurrentDelay.Location = new System.Drawing.Point(12, 81);
+            this.lblCurrentDelay.Name = "lblCurrentDelay";
+            this.lblCurrentDelay.Size = new System.Drawing.Size(363, 108);
+            this.lblCurrentDelay.TabIndex = 4;
+            this.lblCurrentDelay.Text = "000000";
+            // 
+            // btnBuild
+            // 
+            this.btnBuild.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnBuild.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuild.ForeColor = System.Drawing.Color.White;
+            this.btnBuild.Location = new System.Drawing.Point(30, 236);
+            this.btnBuild.Name = "btnBuild";
+            this.btnBuild.Size = new System.Drawing.Size(75, 64);
+            this.btnBuild.TabIndex = 5;
+            this.btnBuild.Text = "Up";
+            this.btnBuild.UseVisualStyleBackColor = false;
+            this.btnBuild.Click += new System.EventHandler(this.btnBuild_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Olive;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(111, 236);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 64);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Down";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnDump
+            // 
+            this.btnDump.BackColor = System.Drawing.Color.Maroon;
+            this.btnDump.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDump.ForeColor = System.Drawing.Color.White;
+            this.btnDump.Location = new System.Drawing.Point(273, 236);
+            this.btnDump.Name = "btnDump";
+            this.btnDump.Size = new System.Drawing.Size(75, 64);
+            this.btnDump.TabIndex = 8;
+            this.btnDump.Text = "DUMP";
+            this.btnDump.UseVisualStyleBackColor = false;
+            this.btnDump.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnDump_Click);
+            this.btnDump.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnCough_MouseDown);
+            this.btnDump.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnCough_MouseUp);
+            // 
+            // inputSelector
+            // 
+            this.inputSelector.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.inputSelector.FormattingEnabled = true;
+            this.inputSelector.Location = new System.Drawing.Point(86, 332);
+            this.inputSelector.Name = "inputSelector";
+            this.inputSelector.Size = new System.Drawing.Size(262, 21);
+            this.inputSelector.TabIndex = 9;
+            this.inputSelector.SelectedIndexChanged += new System.EventHandler(this.inputSelector_SelectedIndexChanged);
+            // 
+            // outputSelector
+            // 
+            this.outputSelector.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.outputSelector.FormattingEnabled = true;
+            this.outputSelector.Location = new System.Drawing.Point(86, 360);
+            this.outputSelector.Name = "outputSelector";
+            this.outputSelector.Size = new System.Drawing.Size(262, 21);
+            this.outputSelector.TabIndex = 10;
+            this.outputSelector.SelectedIndexChanged += new System.EventHandler(this.outputSelector_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(49, 335);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Input";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(41, 363);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Output";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // txtDumps
+            // 
+            this.txtDumps.Enabled = false;
+            this.txtDumps.Location = new System.Drawing.Point(308, 306);
+            this.txtDumps.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.txtDumps.Name = "txtDumps";
+            this.txtDumps.Size = new System.Drawing.Size(40, 20);
+            this.txtDumps.TabIndex = 13;
+            this.txtDumps.UseWaitCursor = true;
+            this.txtDumps.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.txtDumps.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(262, 308);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Dumps";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(30, 193);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(318, 23);
+            this.progressBar1.TabIndex = 15;
+            // 
+            // btnCough
+            // 
+            this.btnCough.BackColor = System.Drawing.Color.DarkBlue;
+            this.btnCough.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCough.ForeColor = System.Drawing.Color.White;
+            this.btnCough.Location = new System.Drawing.Point(192, 236);
+            this.btnCough.Name = "btnCough";
+            this.btnCough.Size = new System.Drawing.Size(75, 64);
+            this.btnCough.TabIndex = 16;
+            this.btnCough.Text = "Cough";
+            this.btnCough.UseVisualStyleBackColor = false;
+            this.btnCough.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnCough_MouseDown);
+            this.btnCough.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnCough_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 388);
+            this.Controls.Add(this.btnCough);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDumps);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.outputSelector);
+            this.Controls.Add(this.inputSelector);
+            this.Controls.Add(this.btnDump);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnBuild);
+            this.Controls.Add(this.lblCurrentDelay);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSpeed);
+            this.Controls.Add(this.txtTarget);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.txtTarget)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtSpeed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDumps)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown txtTarget;
+        private System.Windows.Forms.NumericUpDown txtSpeed;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCurrentDelay;
+        private System.Windows.Forms.Button btnBuild;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnDump;
+        private System.Windows.Forms.ComboBox inputSelector;
+        private System.Windows.Forms.ComboBox outputSelector;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown txtDumps;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnCough;
     }
 }
 
--- Delay/Form1.cs
+++ Delay/Form1.cs
@@ -7,14 +7,332 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
 
 namespace Delay
 {
     public partial class Form1 : Form
     {
+        WaveFormat waveformat = new WaveFormat(44100, 16, 2);
+        BufferedWaveProvider buffer;
+        WaveOutEvent output = new WaveOutEvent();
+        WaveInEvent input = new WaveInEvent();
+
+        bool targetRampedUp = false;
+        bool rampingup = false;
+        bool rampingdown = false;
+        int targetMs = 20000;
+        int rampSpeed = 2;
+        int curdelay;
+        int buffavg = 0;
+        int buffavgcounter = 0;
+        int dumpMs = 0;
+        
+        
+
+
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int n = 0; n < WaveOut.DeviceCount; n++)
+            {
+                var caps = WaveOut.GetCapabilities(n);
+                outputSelector.Items.Add($"{caps.ProductName}");
+            }
+            for (int n = 0; n < WaveIn.DeviceCount; n++)
+            {
+                var caps = WaveIn.GetCapabilities(n);
+                inputSelector.Items.Add($"{caps.ProductName}");
+            }
+            txtTarget.DecimalPlaces = 1;
+            buffer = new BufferedWaveProvider(waveformat);
+            buffer.BufferDuration = new TimeSpan(1, 0, 0);
+            inputSelector.SelectedIndex = 0;
+            outputSelector.SelectedIndex = 0;
+            dumpMs = (int)(targetMs / txtDumps.Value);
+            InitializeAudio();
+        }
+
+        private void InitializeAudio()
+        {
+            input.Dispose();
+            output.Dispose();
+            output = new WaveOutEvent();
+            input = new WaveInEvent();
+            input.DeviceNumber = inputSelector.SelectedIndex;
+            output.DeviceNumber = outputSelector.SelectedIndex;
+
+            
+            input.WaveFormat = waveformat;
+            
+            input.DataAvailable += new EventHandler<WaveInEventArgs>(DataAvailable);
+            
+            output.Init(buffer);
+            output.Pause();
+            try
+            {
+                input.StartRecording();
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void DataAvailable(object sender, WaveInEventArgs e)
+        {
+            
+            
+
+            if ((targetRampedUp && rampingup && curdelay < targetMs))
+            {
+                var stretchedbuffer = stretch(e.Buffer, (1.00 + (rampSpeed/100.0)));
+                buffer.AddSamples(stretchedbuffer, 0, stretchedbuffer.Length);
+                //ramping = false;
+                if (curdelay >= targetMs)
+                {
+                    rampingup = false;
+                }
+            }
+            
+            else if ((curdelay > targetMs || !targetRampedUp) && rampingdown && curdelay > output.DesiredLatency)
+            {
+                //Ramp down to the target
+                var stretchedbuffer = stretch(e.Buffer, (1.00 - (rampSpeed / 100.0)));
+                buffer.AddSamples(stretchedbuffer, 0, stretchedbuffer.Length);
+                if ((curdelay <= targetMs && targetRampedUp)||curdelay <= output.DesiredLatency)
+                {
+                    rampingdown = false;
+                }
+            }
+            else
+            {
+                buffer.AddSamples(stretch(e.Buffer,1.00),0,e.BytesRecorded);
+            }
+            if (targetRampedUp && curdelay >= targetMs)
+            {
+                rampingup = false;
+            }
+            else if ((curdelay <= targetMs && targetRampedUp) || curdelay <= output.DesiredLatency)
+            {
+                rampingdown = false;
+            }
+            if (buffer.BufferedDuration.TotalMilliseconds > output.DesiredLatency)
+            {
+                output.Play();
+            }
+            
+
+            
+            
+        }
+
+        private void outputSelector_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            InitializeAudio();
+        }
+
+        private void inputSelector_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            InitializeAudio();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            curdelay = (int)buffer.BufferedDuration.TotalMilliseconds;
+            if (buffer != null)
+            {
+                buffavg += curdelay;
+                buffavgcounter++;
+                if (buffavgcounter == 5)
+                {
+                    lblCurrentDelay.Text = new TimeSpan(0, 0, 0, 0, curdelay).ToString(@"mm\:ss\.f");
+                    buffavgcounter = 0;
+                    buffavg = 0;
+                }
+                               
+            }
+            if (rampingup || rampingdown)
+            {
+                if (rampingdown)
+                {
+                    //we are ramping down
+                    btnBuild.BackColor = Color.DarkGreen;
+                    if (btnExit.BackColor == Color.Yellow)
+                    {
+                        btnExit.BackColor = Color.Olive;
+                    }
+                    else
+                    {
+                        btnExit.BackColor = Color.Yellow;
+                    }
+                }
+                else if (rampingup)
+                {
+                    //we are ramping up
+                    btnExit.BackColor = Color.Olive;
+                    if (btnBuild.BackColor == Color.Lime)
+                    {
+                        btnBuild.BackColor = Color.DarkGreen;
+                    }
+                    else
+                    {
+                        btnBuild.BackColor = Color.Lime;
+                    }
+                }
+            }
+            else
+            {
+                btnBuild.BackColor = Color.DarkGreen;
+                btnExit.BackColor = Color.Olive;
+            }
+            if (curdelay > dumpMs - output.DesiredLatency)
+            {
+                btnDump.BackColor = Color.Red;
+            }
+            else
+            {
+                btnDump.BackColor = Color.DarkRed;
+            }
+            if (targetMs < output.DesiredLatency)
+            {
+                targetMs = output.DesiredLatency;
+            }
+            progressBar1.Maximum = targetMs;
+            if (curdelay <= targetMs)
+            {
+                progressBar1.Value = curdelay;
+            }
+            else
+            {
+                progressBar1.Value = targetMs;
+            }
+
+            
+        }
+
+        private void txtTarget_ValueChanged(object sender, EventArgs e)
+        {
+            targetMs = (int)(txtTarget.Value*1000);
+            
+            if (targetRampedUp && curdelay > targetMs)
+            {
+                rampingdown = true;
+                rampingup = false;
+            }
+            else if (targetRampedUp && curdelay < targetMs)
+            {
+                rampingup = true;
+                rampingdown = false;
+            }
+            dumpMs = (int)(targetMs / txtDumps.Value);
+        }
+
+        private void txtSpeed_ValueChanged(object sender, EventArgs e)
+        {
+            rampSpeed = (int)txtSpeed.Value;
+        }
+
+        private void btnBuild_Click(object sender, EventArgs e)
+        {
+            targetMs = (int)(txtTarget.Value * 1000);
+            targetRampedUp = true;
+            rampingdown = false;
+            rampingup = true;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            targetRampedUp = false;
+            //targetMs = output.DesiredLatency;
+            rampingdown = true;
+            rampingup = false;
+        }
+
+        
+        
+              
+
+        private byte[] stretch(byte[] inputbytes, double stretchfactor)
+        {
+            int blockalign = input.WaveFormat.BlockAlign;
+            int outputblocks = (int)((inputbytes.Length / blockalign) * stretchfactor);
+            byte[] outputbytes = new byte[outputblocks* blockalign];
+            
+            byte[][] inputblocks = new byte[inputbytes.Length / blockalign][];
+            for (int i = 0; i < inputblocks.Length; i++)
+            {
+                byte[] block = new byte[blockalign];
+                for (int j = 0; j < blockalign; j++)
+                {
+                    block[j] = inputbytes[(i * blockalign) + j];
+                }
+                inputblocks[i] = block;
+            }
+            for (int i = 0; i < outputbytes.Length; i+=blockalign)
+            {
+                int blocktarget = (int)(((float)i / outputbytes.Length) * inputblocks.Length);
+                for (int j = 0; j < blockalign; j++)
+                {
+                    outputbytes[i + j] = inputblocks[blocktarget][j];
+                }
+            }
+            
+            return outputbytes;
+        }
+
+        private void btnDump_Click(object sender, EventArgs e)
+        {
+            output.Pause();
+            var tempbuffer = buffer;
+            int dumpbytes = (dumpMs / 1000) * waveformat.SampleRate * (waveformat.BitsPerSample / 8);
+            if (tempbuffer.BufferedBytes > dumpbytes)
+            {
+                byte[] newBuffer = new byte[tempbuffer.BufferedBytes - dumpbytes];
+                for (int i = 0; i < newBuffer.Length; i++)
+                {
+                    
+                }
+            }
+            buffer.ClearBuffer();
+
+            
+            
+            if (targetRampedUp && curdelay < targetMs)
+            {
+                rampingup = true;
+            }
+            else
+            {
+                rampingdown = true;
+            }
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            dumpMs = (int)(targetMs / txtDumps.Value);
+        }
+
+        private void btnCough_MouseDown(object sender, EventArgs e)
+        {
+            input.StopRecording();
+            btnCough.BackColor = Color.Blue;
+        }
+        private void btnCough_MouseUp(object sender, EventArgs e)
+        {
+            input.StartRecording();
+            btnCough.BackColor = Color.DarkBlue;
+            if(targetRampedUp && curdelay < targetMs)
+            {
+                rampingup = true;
+            }
+        }
+
     }
 }
