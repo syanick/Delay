e54e2ccea23b95a1cad55d3c2c0b1d6c2ead3d6d
k2fc <dennis.graiani@gmail.com>
2019-10-15 13:11:18 -0400

    Attempt to fix sample calculations

--- Delay/Form1.cs
+++ Delay/Form1.cs
@@ -477,8 +477,8 @@ namespace Delay
             float[][] inputSamples = BytesToSamples(inputbytes, waveformat);
             float[][] outputSamples = new float[inputSamples.Length][];
 
-            avgLevel = 6 + dBFS(AvgAudioLevel(inputSamples));
-            peakLevel = 6 + dBFS(PeakAudioLevel(inputSamples));
+            avgLevel = dBFS(AvgAudioLevel(inputSamples));
+            peakLevel = dBFS(PeakAudioLevel(inputSamples));
 
             if (avgLevel > silenceThreshold)
             {
@@ -557,7 +557,7 @@ namespace Delay
                     }
                     sample += monosample / waveformat.Channels;
                 }
-                samples[i] = sample / ((float)Math.Pow(2, waveformat.BitsPerSample));
+                samples[i] = sample / ((float)Math.Pow(2, waveformat.BitsPerSample - 1));
             }
             return samples;
         }
@@ -576,12 +576,12 @@ namespace Delay
                         monosample = monosample << 8;
                         monosample += buffer[(j * waveformat.BlockAlign) + (i * (waveformat.BlockAlign / waveformat.Channels)) + (k - 1)];
                     }
-                    int maxint = ((int)Math.Pow(2, waveformat.BitsPerSample) / 2) - 1;
-                    if (monosample > maxint)
+                    int maxint = ((int)Math.Pow(2, waveformat.BitsPerSample) / 2);
+                    if (monosample >= maxint)
                     {
-                        monosample = -1 - ((maxint + 1) - (monosample - maxint));
+                        monosample = 0 - (maxint - (monosample & (maxint-1)));
                     }
-                    channel[j] = monosample / ((float)Math.Pow(2, waveformat.BitsPerSample));
+                    channel[j] = monosample / (float)maxint;
                 }
                 samples[i] = channel;
             }
@@ -596,7 +596,8 @@ namespace Delay
             {
                 for (int j = 0; j < samples[0].Length; j++)
                 {
-                    int monosample = (int)(samples[i][j] * ((float)Math.Pow(2, waveformat.BitsPerSample)));
+                    int maxint = ((int)Math.Pow(2, waveformat.BitsPerSample) / 2);
+                    int monosample = (int)(samples[i][j] * maxint);
                     //int k = waveformat.BitsPerSample / 8; k > 0; k--
                     for (int k = 0; k < waveformat.BlockAlign / waveformat.Channels; k++)
                     {
