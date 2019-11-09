bd29103971626c9c3a2365c171a13bed361b2643
syanick <syanick@gmail.com>
2019-11-09 18:35:21 -0500

    more efficient multi-dump

--- Delay/Form1.cs
+++ Delay/Form1.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using System.Windows.Forms;
 using NAudio.Wave;
 using NAudio.Dsp;
+using NAudio.Utils;
 using soundtouch;
 
 namespace Delay
@@ -10,8 +11,9 @@ namespace Delay
     public partial class Form1 : Form
     {
         WaveFormat waveformat = new WaveFormat(44100, 16, 2);
-        BufferedWaveProvider buffer;
-        BufferedWaveProvider repeatBuffer;
+        BufferedDelayProvider buffer;
+        BufferedDelayProvider outbuffer;
+        BufferedDelayProvider repeatBuffer;
         WaveOutEvent output = new WaveOutEvent();
         WaveInEvent input = new WaveInEvent();
         SoundTouch stretcher = new SoundTouch();
@@ -76,9 +78,11 @@ namespace Delay
                 inputSelector.Items.Add($"{caps.ProductName}");
             }
             txtTarget.DecimalPlaces = 1;
-            buffer = new BufferedWaveProvider(waveformat);
-            repeatBuffer = new BufferedWaveProvider(waveformat);
+            buffer = new BufferedDelayProvider(waveformat);
+            outbuffer = new BufferedDelayProvider(waveformat);
+            repeatBuffer = new BufferedDelayProvider(waveformat);
             buffer.BufferDuration = new TimeSpan(1, 1, 0);
+            outbuffer.BufferDuration = new TimeSpan(0, 2, 0);
             repeatBuffer.BufferDuration = new TimeSpan(1, 1, 0);
             inputSelector.SelectedIndex = 0;
             outputSelector.SelectedIndex = 0;
@@ -465,8 +469,8 @@ namespace Delay
         private void timer1_Tick(object sender, EventArgs e)
         {
             double realRampSpeedPercent = 1.00 * realRampSpeed / realRampFactor;
-            //lblDebug1.Text = endRampTime.ToString();
-            lblDebug1.Text = tempochange.ToString();
+            lblDebug1.Text = endRampTime.ToString();
+            //lblDebug1.Text = tempochange.ToString();
             lblDebug2.Text = realRampSpeedPercent.ToString() + "%";
             lblDebug3.Text = peakLevel.ToString("F") + " dB";
             lblDebug4.Text = avgLevel.ToString("F") + " dB";
@@ -490,7 +494,14 @@ namespace Delay
                 if (buffavgcounter == 5)
                 {
                     buffavg = buffcumulative / buffavgcounter;
-                    lblCurrentDelay.Text = new TimeSpan(0, 0, 0, 0, buffavg).ToString(@"mm\:ss\.f");
+                    if(buffavg >= 3600000)
+                    {
+                        lblCurrentDelay.Text = new TimeSpan(0, 0, 0, 0, buffavg).ToString(@"h\:mm\:ss");
+                    }
+                    else
+                    {
+                        lblCurrentDelay.Text = new TimeSpan(0, 0, 0, 0, buffavg).ToString(@"mm\:ss\.f");
+                    }
                     buffavgcounter = 0;
                     buffcumulative = 0;
                     if (buffavg > dumpMs - output.DesiredLatency)
@@ -906,27 +917,31 @@ namespace Delay
             input.StopRecording();
             recording = false;
 
-            int tempbufferbytes;
+            //int tempbufferbytes;
+            int dumpbytes;
 
             if (curdelay > dumpMs && dumps > 1)
             {
-                tempbufferbytes = buffer.BufferedBytes - ((waveformat.AverageBytesPerSecond * (targetMs / 1000) / dumps));//* (dumps - 1) / dumps / waveformat.BlockAlign * waveformat.BlockAlign;
-                var tempbuffer = new byte[buffer.BufferedBytes];
+                //tempbufferbytes = buffer.BufferedBytes - ((waveformat.AverageBytesPerSecond * (targetMs / 1000) / dumps));//* (dumps - 1) / dumps / waveformat.BlockAlign * waveformat.BlockAlign;
+                dumpbytes = (waveformat.AverageBytesPerSecond * (targetMs / 1000) / dumps);
+                //var tempbuffer = new byte[buffer.BufferedBytes];
 
                 if(me.Button != System.Windows.Forms.MouseButtons.Right)
                 {
-                    tempbufferbytes = buffer.Read(tempbuffer, 0, tempbufferbytes);
-                    buffer.ClearBuffer();
-                    buffer.AddSamples(tempbuffer, 0, tempbufferbytes);
+                    //tempbufferbytes = buffer.Read(tempbuffer, 0, tempbufferbytes);
+                    //buffer.ClearBuffer();
+                    //buffer.AddSamples(tempbuffer, 0, tempbufferbytes);
+                    buffer.Dump(dumpbytes);
                 }
                 else
                 {
-                    int tempoffset = (waveformat.AverageBytesPerSecond * (targetMs / 1000) / dumps);
-                    var tempbuffer2 = new byte[tempbufferbytes];
-                    buffer.Read(tempbuffer, 0, tempbuffer.Length);
-                    Buffer.BlockCopy(tempbuffer, tempoffset, tempbuffer2, 0, tempbufferbytes);
-                    buffer.ClearBuffer();
-                    buffer.AddSamples(tempbuffer2, 0, tempbufferbytes);
+                    //int tempoffset = (waveformat.AverageBytesPerSecond * (targetMs / 1000) / dumps);
+                    //var tempbuffer2 = new byte[tempbufferbytes];
+                    //buffer.Read(tempbuffer, 0, tempbuffer.Length);
+                    //Buffer.BlockCopy(tempbuffer, tempoffset, tempbuffer2, 0, tempbufferbytes);
+                    //buffer.ClearBuffer();
+                    //buffer.AddSamples(tempbuffer2, 0, tempbufferbytes);
+                    buffer.DumpEnd(dumpbytes);
                 }
 
             }
@@ -1254,6 +1269,320 @@ namespace Delay
                 timeMode = false;
                 repeatMode = true;
             }
+        }
+    }
+
+    /// <summary>
+    /// Modified version of CircularBuffer from NAudio
+    /// A very basic circular buffer implementation
+    /// </summary>
+    public class CircularDelayBuffer
+    {
+        private readonly byte[] buffer;
+        private readonly object lockObject;
+        private int writePosition;
+        private int readPosition;
+        private int byteCount;
+
+        /// <summary>
+        /// Create a new circular buffer
+        /// </summary>
+        /// <param name="size">Max buffer size in bytes</param>
+        public CircularDelayBuffer(int size)
+        {
+            buffer = new byte[size];
+            lockObject = new object();
+        }
+
+        /// <summary>
+        /// Write data to the buffer
+        /// </summary>
+        /// <param name="data">Data to write</param>
+        /// <param name="offset">Offset into data</param>
+        /// <param name="count">Number of bytes to write</param>
+        /// <returns>number of bytes written</returns>
+        public int Write(byte[] data, int offset, int count)
+        {
+            lock (lockObject)
+            {
+                var bytesWritten = 0;
+                if (count > buffer.Length - byteCount)
+                {
+                    count = buffer.Length - byteCount;
+                }
+                // write to end
+                int writeToEnd = Math.Min(buffer.Length - writePosition, count);
+                Array.Copy(data, offset, buffer, writePosition, writeToEnd);
+                writePosition += writeToEnd;
+                writePosition %= buffer.Length;
+                bytesWritten += writeToEnd;
+                if (bytesWritten < count)
+                {
+                    //Debug.Assert(writePosition == 0);
+                    // must have wrapped round. Write to start
+                    Array.Copy(data, offset + bytesWritten, buffer, writePosition, count - bytesWritten);
+                    writePosition += (count - bytesWritten);
+                    bytesWritten = count;
+                }
+                byteCount += bytesWritten;
+                return bytesWritten;
+            }
+        }
+
+        /// <summary>
+        /// Read from the buffer
+        /// </summary>
+        /// <param name="data">Buffer to read into</param>
+        /// <param name="offset">Offset into read buffer</param>
+        /// <param name="count">Bytes to read</param>
+        /// <returns>Number of bytes actually read</returns>
+        public int Read(byte[] data, int offset, int count)
+        {
+            lock (lockObject)
+            {
+                if (count > byteCount)
+                {
+                    count = byteCount;
+                }
+                int bytesRead = 0;
+                int readToEnd = Math.Min(buffer.Length - readPosition, count);
+                Array.Copy(buffer, readPosition, data, offset, readToEnd);
+                bytesRead += readToEnd;
+                readPosition += readToEnd;
+                readPosition %= buffer.Length;
+
+                if (bytesRead < count)
+                {
+                    // must have wrapped round. Read from start
+                    //Debug.Assert(readPosition == 0);
+                    Array.Copy(buffer, readPosition, data, offset + bytesRead, count - bytesRead);
+                    readPosition += (count - bytesRead);
+                    bytesRead = count;
+                }
+
+                byteCount -= bytesRead;
+                //Debug.Assert(byteCount >= 0);
+                return bytesRead;
+            }
+        }
+
+        /// <summary>
+        /// Maximum length of this circular buffer
+        /// </summary>
+        public int MaxLength => buffer.Length;
+
+        /// <summary>
+        /// Number of bytes currently stored in the circular buffer
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                lock (lockObject)
+                {
+                    return byteCount;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Resets the buffer
+        /// </summary>
+        public void Reset()
+        {
+            lock (lockObject)
+            {
+                ResetInner();
+            }
+        }
+
+        private void ResetInner()
+        {
+            byteCount = 0;
+            readPosition = 0;
+            writePosition = 0;
+        }
+
+        /// <summary>
+        /// Advances the buffer, discarding bytes
+        /// </summary>
+        /// <param name="count">Bytes to advance</param>
+        public void Advance(int count)
+        {
+            lock (lockObject)
+            {
+                if (count >= byteCount)
+                {
+                    ResetInner();
+                }
+                else
+                {
+                    byteCount -= count;
+                    readPosition += count;
+                    readPosition %= MaxLength;
+                }
+            }
+        }
+
+        public void Dump(int count)
+        {
+            lock (lockObject)
+            {
+                if (count >= byteCount)
+                {
+                    ResetInner();
+                }
+                else
+                {
+                    byteCount -= count;
+                    writePosition -= count;
+                    if(writePosition < 0)
+                    {
+                        writePosition += MaxLength;
+                    }
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// This is a modified verison of NAudio's BufferedWaveProvider
+    /// Provides a buffered store of samples
+    /// Read method will return queued samples or fill buffer with zeroes
+    /// Now backed by a circular buffer
+    /// </summary>
+    public class BufferedDelayProvider : IWaveProvider
+    {
+        private CircularDelayBuffer circularBuffer;
+        private readonly WaveFormat waveFormat;
+
+        /// <summary>
+        /// Creates a new buffered WaveProvider
+        /// </summary>
+        /// <param name="waveFormat">WaveFormat</param>
+        public BufferedDelayProvider(WaveFormat waveFormat)
+        {
+            this.waveFormat = waveFormat;
+            BufferLength = waveFormat.AverageBytesPerSecond * 5;
+            ReadFully = true;
+        }
+
+        /// <summary>
+        /// If true, always read the amount of data requested, padding with zeroes if necessary
+        /// By default is set to true
+        /// </summary>
+        public bool ReadFully { get; set; }
+
+        /// <summary>
+        /// Buffer length in bytes
+        /// </summary>
+        public int BufferLength { get; set; }
+
+        /// <summary>
+        /// Buffer duration
+        /// </summary>
+        public TimeSpan BufferDuration
+        {
+            get
+            {
+                return TimeSpan.FromSeconds((double)BufferLength / WaveFormat.AverageBytesPerSecond);
+            }
+            set
+            {
+                BufferLength = (int)(value.TotalSeconds * WaveFormat.AverageBytesPerSecond);
+            }
+        }
+
+        /// <summary>
+        /// If true, when the buffer is full, start throwing away data
+        /// if false, AddSamples will throw an exception when buffer is full
+        /// </summary>
+        public bool DiscardOnBufferOverflow { get; set; }
+
+        /// <summary>
+        /// The number of buffered bytes
+        /// </summary>
+        public int BufferedBytes
+        {
+            get
+            {
+                return circularBuffer == null ? 0 : circularBuffer.Count;
+            }
+        }
+
+        /// <summary>
+        /// Buffered Duration
+        /// </summary>
+        public TimeSpan BufferedDuration
+        {
+            get { return TimeSpan.FromSeconds((double)BufferedBytes / WaveFormat.AverageBytesPerSecond); }
+        }
+
+        /// <summary>
+        /// Gets the WaveFormat
+        /// </summary>
+        public WaveFormat WaveFormat
+        {
+            get { return waveFormat; }
+        }
+
+        /// <summary>
+        /// Adds samples. Takes a copy of buffer, so that buffer can be reused if necessary
+        /// </summary>
+        public void AddSamples(byte[] buffer, int offset, int count)
+        {
+            // create buffer here to allow user to customise buffer length
+            if (circularBuffer == null)
+            {
+                circularBuffer = new CircularDelayBuffer(BufferLength);
+            }
+
+            var written = circularBuffer.Write(buffer, offset, count);
+            if (written < count && !DiscardOnBufferOverflow)
+            {
+                throw new InvalidOperationException("Buffer full");
+            }
+        }
+
+        /// <summary>
+        /// Reads from this WaveProvider
+        /// Will always return count bytes, since we will zero-fill the buffer if not enough available
+        /// </summary>
+        public int Read(byte[] buffer, int offset, int count)
+        {
+            int read = 0;
+            if (circularBuffer != null) // not yet created
+            {
+                read = circularBuffer.Read(buffer, offset, count);
+            }
+            if (ReadFully && read < count)
+            {
+                // zero the end of the buffer
+                Array.Clear(buffer, offset + read, count - read);
+                read = count;
+            }
+            return read;
+        }
+
+        /// <summary>
+        /// Discards all audio from the buffer
+        /// </summary>
+        public void ClearBuffer()
+        {
+            if (circularBuffer != null)
+            {
+                circularBuffer.Reset();
+            }
+        }
+
+        public void Dump(int bytes)
+        {
+            circularBuffer.Dump(bytes);
+        }
+
+        public void DumpEnd(int bytes)
+        {
+            circularBuffer.Advance(bytes);
         }
     }
 }
